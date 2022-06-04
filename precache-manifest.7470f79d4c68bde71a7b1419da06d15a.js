e208dfb4acc5dced8c121022e4682d690e576f9b
Kalle Chen <kallechen.tw@gmail.com>
2022-06-04 14:52:55 +0800

    Updates

---
+++ precache-manifest.7470f79d4c68bde71a7b1419da06d15a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0565c647db5a575291e2d0c563f45e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "794b5aa940097ab7916b",
+    "url": "/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "eac1b9b382762f44a937",
+    "url": "/static/css/main.72e64c5b.chunk.css"
+  },
+  {
+    "revision": "794b5aa940097ab7916b",
+    "url": "/static/js/2.e2e02d65.chunk.js"
+  },
+  {
+    "revision": "0297ac78c823ef9b172e38637e06e92e",
+    "url": "/static/js/2.e2e02d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac1b9b382762f44a937",
+    "url": "/static/js/main.6a4cea0d.chunk.js"
+  },
+  {
+    "revision": "96686fea9617cccce352",
+    "url": "/static/js/runtime-main.6ec5081d.js"
+  }
+]);
