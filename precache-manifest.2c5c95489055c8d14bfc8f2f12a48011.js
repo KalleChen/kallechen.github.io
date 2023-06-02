bd61a94e4ae395d4e26a1f810794369e24759217
KalleChen <KalleChen@users.noreply.github.com>
2023-06-02 11:28:24 +0000

    deploy: 9b466abc9216589122754e1311841529f5554721

---
+++ precache-manifest.2c5c95489055c8d14bfc8f2f12a48011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335b51db8a3d7db877178304c121787a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5855e8a9f2352fec0b",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b0821eb54e6c7a961b62",
+    "url": "/static/css/main.e043677a.chunk.css"
+  },
+  {
+    "revision": "fa5855e8a9f2352fec0b",
+    "url": "/static/js/2.7a97e630.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.7a97e630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0821eb54e6c7a961b62",
+    "url": "/static/js/main.83d1a75c.chunk.js"
+  },
+  {
+    "revision": "96686fea9617cccce352",
+    "url": "/static/js/runtime-main.6ec5081d.js"
+  }
+]);
